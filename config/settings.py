d014c0862fca2f8f571272a8cc8a945e2b3eb29d
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-04-29 17:23:52 +0500

    q

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
-MEDIA_URL = 'media/'
+MEDIA_URL = 'media/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
