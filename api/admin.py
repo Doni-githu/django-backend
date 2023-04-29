fa8189a6c23916fd5d56957e82bf4acbb1db72b6
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-04-29 16:03:57 +0500

    v

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
 from .models import Buy, ChatGlobal, ChatOnly, Comentariya, Product, Account
 # Register your models here.
-admin.site.register([Buy, ChatGlobal, ChatOnly, Comentariya, Product, Account])
--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
@@ -22,7 +21,6 @@ from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('login', obtain_auth_token),
     path('', include('api.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
