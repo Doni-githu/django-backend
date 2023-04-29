1641ad857141be73de4dfafa7bd1661a0ae664d8
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-04-29 15:48:36 +0500

    fix

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login', obtain_auth_token),
     path('', include('api.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+
+
+urlpatterns +=staticfiles_urlpatterns()
